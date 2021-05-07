8781e8ae874b2ea5ec26bea3add290ed71f4695c
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-05-07 15:17:24 +0530

    App ready

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jabme.app.wsgi --log-file -
--- jabme/app/settings/prod.py
+++ jabme/app/settings/prod.py
@@ -1,12 +1,18 @@
 import os
 
+import dj_database_url
+
 from jabme.app.settings.base import *  # noqa
 
 SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY", "p7g_g$a7^qfn!zxc&h+fbvy*0+@trz7)pkr#54elu!1u#rg@tq"
 )
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["jabme.herokuapp.com"]
 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
+# database config
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)  # noqa
