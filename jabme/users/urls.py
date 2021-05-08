16414b254bfe83008b9c52f00f2384873d40e84d
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-05-08 21:13:59 +0530

    Basic Iteration backend doneo

--- jabme/users/urls.py
+++ jabme/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from jabme.users.views import RegisterView
+from jabme.users.views import FindSlotView, RegisterView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("slots/", FindSlotView.as_view(), name="slots"),
 ]
