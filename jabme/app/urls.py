ac2809079b1480cc7b978961c8fc5c718b937a3b
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-05-12 11:48:31 +0530

    Django-react-ssr done

--- jabme/app/urls.py
+++ jabme/app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("jabme.users.urls")),
+    path("", TemplateView.as_view(template_name="index.html")),
 ]
