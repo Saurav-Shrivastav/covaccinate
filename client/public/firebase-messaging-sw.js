7a984e0f6a3eab96c5dbbbe0f83efd1a8392cba2
aniketbiswas21 <aniket.biswas75@gmail.com>
2021-05-12 15:11:23 +0530

    fix: added env variable creation script

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -7,14 +7,14 @@ importScripts("swenv.js");
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: swprocess.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: swprocess.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: `https://${swprocess.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
+  projectId: swprocess.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: swprocess.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: swprocess.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: swprocess.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: swprocess.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(config);
