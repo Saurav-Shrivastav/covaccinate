12513e811f3e45d6de9b114d2bbd1c387d01ff8b
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-05-13 21:44:35 +0530

    fix CI

--- jabme/app/settings/prod.py
+++ jabme/app/settings/prod.py
@@ -8,7 +8,9 @@ SECRET_KEY = os.environ.get(
     "SECRET_KEY", "p7g_g$a7^qfn!zxc&h+fbvy*0+@trz7)pkr#54elu!1u#rg@tq"
 )
 DEBUG = int(os.environ.get("DEBUG", default=0))
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get(
+    "DJANGO_ALLOWED_HOSTS", "127.0.0.1 localhost"
+).split(" ")
 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
