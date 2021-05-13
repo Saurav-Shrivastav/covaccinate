eae52c46a5f25cb3d2cc6e2cc929eefb70fb28d4
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-05-13 16:12:29 +0530

    Dockerfile and docker-compose for web and postgres added

--- jabme/app/settings/prod.py
+++ jabme/app/settings/prod.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get(
     "DJANGO_SECRET_KEY", "p7g_g$a7^qfn!zxc&h+fbvy*0+@trz7)pkr#54elu!1u#rg@tq"
 )
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-ALLOWED_HOSTS = ["jabme.herokuapp.com"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
