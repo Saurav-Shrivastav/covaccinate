70454d165493d7d4df3451b79e4639138da8348d
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-05-13 20:33:07 +0530

    nginx conf

--- jabme/app/settings/base.py
+++ jabme/app/settings/base.py
@@ -59,8 +59,14 @@ AUTH_USER_MODEL = "users.User"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get(
+            "SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")
+        ),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
--- jabme/app/settings/dev.py
+++ jabme/app/settings/dev.py
@@ -3,13 +3,13 @@ import os
 import environ
 from app.settings.base import *  # noqa
 
-env = environ.Env(DEBUG=(bool, False))
+env = environ.Env()
 # reading .env file
-environ.Env.read_env(env_file=os.path.join(BASE_DIR, "./.env"))  # noqa
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, "./.env.dev"))  # noqa
 
 
 # False if not in env
-DEBUG = env("DEBUG")
+DEBUG = int(env("DEBUG"))
 SECRET_KEY = env("SECRET_KEY")
 ALLOWED_HOSTS = []
 
