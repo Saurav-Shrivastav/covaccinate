967a5c83e0ed4dab9b1eca01e0681da7544a6ae3
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-05-24 13:14:10 +0530

    static files in django admin work now

--- jabme/app/settings/base.py
+++ jabme/app/settings/base.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
