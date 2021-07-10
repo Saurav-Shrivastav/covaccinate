f2939c05ad6059e2f10a6e305740bf9128822820
aniketbiswas21 <aniket.biswas75@gmail.com>
2021-07-10 18:27:49 +0530

    lazy fix

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { lazy, Suspense } from "react";
 import { Switch } from "react-router-dom";
 import Loading from "pages/Loading/Loading";
+import Register from "pages/Register/Register";
 import RouteWrapper from "./Route";
 
 // * Lazy Imports => Splitting the bundle in multiple parts and only calling them when required. Decreases Webpage loadtime * //
 // * Use only if the component takes more than 30kb in webpack bundle * //
-const Register = lazy(() => import("pages/Register/Register"));
 const VaccineAvailability = lazy(
   () => import("pages/VaccineAvailability/VaccineAvailability")
 );
