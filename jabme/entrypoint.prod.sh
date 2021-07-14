3cfeb59fdf091c54788f873b91c02e46fab2dcf1
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-07-14 22:17:49 +0530

    enable entrypoint.sh to set allowed ips env variable

--- jabme/entrypoint.prod.sh
+++ jabme/entrypoint.prod.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+# if [ "$DATABASE" = "postgres" ]
+# then
+#     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-        sleep 0.1
-    done
+#     while ! nc -z $SQL_HOST $SQL_PORT; do
+#         sleep 0.1
+#     done
 
-    echo "PostgreSQL started"
-fi
+#     echo "PostgreSQL started"
+# fi
 
-exec "$@"
+# exec "$@"
+
+name=$(sudo docker ps -aqf "name=abiswas_be19_mailer")
+container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $name)
+echo $container_ip
+export ALLOWED_IPS=container_ip
