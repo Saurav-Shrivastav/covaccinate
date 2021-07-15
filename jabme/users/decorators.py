a8fb2849e27c30a7580cbd514406bd3588f7de43
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2021-07-15 20:26:03 +0530

    added a validate_header decorator

--- jabme/users/decorators.py
+++ jabme/users/decorators.py
@@ -3,6 +3,7 @@ import os
 from django.http import HttpResponse
 
 allowed_ips = os.environ.get("ALLOWED_IPS", "localhost 127.0.0.1").split()
+super_secret_header = os.environ.get("SECRET_HEADER")
 
 
 def login_by_ip(view_func):
@@ -11,6 +12,18 @@ def login_by_ip(view_func):
         for ip in allowed_ips:
             if ip == user_ip:
                 return view_func(request, *args, **kwargs)
+        return HttpResponse("Forbidden. F Off.")
+
+    return authorize
+
+
+def validate_header(view_func):
+    def authorize(request, *args, **kwargs):
+        if (
+            "only-fan-secret-key" in request.headers
+            and super_secret_header == request.headers["only-fan-secret-key"]
+        ):
+            return view_func(request, *args, **kwargs)
         return HttpResponse("Forbidden. F Off.")
 
     return authorize
